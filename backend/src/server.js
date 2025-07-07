6a33ae2cc2f2cba96bb764d61aea83e7957aabda
TIN PHAM <ptbtin22@clc.fitus.edu.vn>
2025-07-08 01:25:40 +0700

    ready to production

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 import { app, server } from "./lib/socket.js";
 
 import { connectDB } from "./lib/db.js";
@@ -11,6 +12,7 @@ import { connectDB } from "./lib/db.js";
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -18,6 +20,13 @@ app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend/dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
